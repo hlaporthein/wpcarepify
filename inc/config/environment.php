532a035b60ec65b5588f46d895ed7fe896c4b596
hlaporthein <hlaporthein@gmail.com>
2019-03-19 17:45:04 +0630

    php error break
    add missing break php error in environment file

--- inc/config/environment.php
+++ inc/config/environment.php
@@ -9,9 +9,11 @@ define('CP_ENV', 'development'); //development, production, staging
 switch ( CP_ENV ) {
 	case 'development':
 	require_once dirname(__FILE__) .'/environment/development.php';
+	break;
 
 	case 'staging':
 	require_once dirname(__FILE__) .'/environment/staging.php';
+	break;
 
 	default :
 	require_once dirname(__FILE__) .'/environment/production.php';
