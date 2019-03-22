3ad71ef806f2701954fdfc4dd1c3bc156d92cc5a
hlaporthein <hlaporthein@gmail.com>
2019-03-22 13:10:00 +0630

    constant file created
    We would need to create constant file separately in config folder

---
+++ inc/config/constants.php
@@ -0,0 +1,13 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+
+
+// child theme
+define('CP_CHILD_THEME_DIR', get_stylesheet_directory());
+define('CP_CHILD_THEME_URL', get_stylesheet_directory_uri());
+
+
+// parent theme
+define('CP_PARENT_THEME_DIR', get_template_directory());
+define('CP_PARENT_THEME_URL', get_template_directory_uri());
